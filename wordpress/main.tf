d4ff3a4b97bb45790d0d88ac81dbc737701d9063
toshihirock <toshihiro308@gmail.com>
2015-05-16 00:31:05 +0900

    add route table

--- wordpress/main.tf
+++ wordpress/main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_vpc" "main" {
   cidr_block = "10.1.0.0/16"
   enable_dns_hostnames = true
+
   tags {
     Name = "vpc-WordPress"
   }
@@ -15,6 +16,7 @@ resource "aws_subnet" "public-a" {
   vpc_id = "${aws_vpc.main.id}"
   cidr_block = "10.1.11.0/24"
   map_public_ip_on_launch = true
+
   tags {
     Name = "WP-PublicSubnet-A"
   }
@@ -24,7 +26,34 @@ resource "aws_subnet" "private-a" {
   availability_zone = "ap-northeast-1a"
   vpc_id = "${aws_vpc.main.id}"
   cidr_block = "10.1.15.0/24"
+
   tags {
     Name = "WP-PrivateSubnet-A"
   }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags {
+    Name = "WP-InternetGateway"
+  }
+}
+
+resource "aws_route_table" "r" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+  tags {
+    Name = "vpc-WordPress"
+  }
+}
+
+resource "aws_main_route_table_association" "a" {
+    vpc_id = "${aws_vpc.main.id}"
+    route_table_id = "${aws_route_table.r.id}"
 }
