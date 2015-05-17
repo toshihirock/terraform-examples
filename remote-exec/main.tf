e6425aa1676ca817263de7f3c2a6aa424f44dbb3
toshihirock <toshihiro308@gmail.com>
2015-05-18 08:43:36 +0900

    add db

--- remote-exec/main.tf
+++ remote-exec/main.tf
@@ -2,6 +2,21 @@ provider "aws" {
   region = "${var.region}"
 }
 
+resource "aws_db_instance" "default" {
+  identifier = "wp-mysql"
+  allocated_storage = 5
+  engine = "mysql"
+  engine_version = "5.6.22"
+  instance_class = "db.t2.micro"
+  # general purpose SSD
+  storage_type = "gp2"
+  username = "${var.db_name}"
+  password = "${var.db_password}"
+  backup_retention_period = 0
+  vpc_security_group_ids = ["${var.db_security_group_id}"]
+  db_subnet_group_name = "${var.db_subnet_group_name}"
+}
+
 resource "aws_instance" "web" {
   instance_type = "t2.micro"
   ami = "${var.ami}"
--- remote-exec/variables.tf
+++ remote-exec/variables.tf
@@ -1,6 +1,8 @@
 variable "ami" {}
 variable "key_name" {}
 variable "ssh_key_file" {}
+variable "db_security_group_id" {}
+variable "db_subnet_group_name" {}
 variable "region" {
   default = "ap-northeast-1"
 }
