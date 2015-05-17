bb69697d16df9983421e884c4901caaad7c6adb7
toshihirock <toshihiro308@gmail.com>
2015-05-17 18:41:00 +0900

    set up wordpress

--- remote-exec/main.tf
+++ remote-exec/main.tf
@@ -9,7 +9,17 @@ resource "aws_instance" "web" {
   security_groups = ["Web"]
   provisioner "remote-exec" {
     inline = [
-    "touch test.txt"
+    "sudo yum install php php-mysql php-gd php-mbstring -y",
+    "sudo yum install mysql -y",
+    "wget -O /tmp/wordpress-4.1-ja.tar.gz https://ja.wordpress.org/wordpress-4.1-ja.tar.gz",
+    "sudo tar zxf /tmp/wordpress-4.1-ja.tar.gz -C /opt",
+    "sudo ln -s /opt/wordpress /var/www/html/",
+    "sudo chown -R apache:apache /opt/wordpress",
+    "sudo chkconfig httpd on",
+    "sudo killall -9 httpd",
+    "sudo rm -f /var/lock/subsys/httpd",
+    "sudo service httpd start",
+    "sudo service httpd status"
     ]
     connection {
       user = "ec2-user"
