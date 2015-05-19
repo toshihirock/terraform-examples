c331889c0223d44c005c5a16c7d660d81d93c0ed
toshihirock <toshihiro308@gmail.com>
2015-05-19 22:19:33 +0900

    no backup

--- wordpress/main.tf
+++ wordpress/main.tf
@@ -155,6 +155,7 @@ resource "aws_db_instance" "default" {
   storage_type = "gp2"
   username = "${var.db_name}"
   password = "${var.db_password}"
+  backup_retention_period = 0
   vpc_security_group_ids = ["${aws_security_group.db.id}"]
   db_subnet_group_name = "${aws_db_subnet_group.main.name}"
 }
